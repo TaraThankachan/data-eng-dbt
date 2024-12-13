e64ba5333d23cceaa3ff92df90a4db8f46ad352b
Tara Thankachan <taratraicy@gmail.com>
2024-12-13 11:24:00 +0000

    develop stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,21 @@
+select 
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+--from raw customer
+c.c2 as CustomerName,  -- Use exact case
+c.C3 as Segment,
+c.C4 as Country,
+-- from raw product
+p.C1 as category,
+p.C3 as productname,
+p.C4 as subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.C1
+left join {{ ref('raw_product') }} as p
+on o.productid = p.c2
