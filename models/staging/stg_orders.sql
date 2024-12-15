2c8ea6f6b08e7b7cafe762eb4eee994b1546841e
Tara Thankachan <taratraicy@gmail.com>
2024-12-15 14:46:46 +0000

    added delivery_team seed

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,4 +1,5 @@
 select 
+{{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.C1', 'p.C2']) }} as sk_orders,
 o.orderid,
 o.orderdate,
 o.shipdate,
@@ -16,11 +17,14 @@ p.C2 as productid,
 p.C1 as category,
 p.C3 as productname,
 p.C4 as subcategory,
-{{ markup('ordersellingprice','ordercostprice')}} as markup
+{{ markup('ordersellingprice','ordercostprice')}} as markup,
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.C1
 left join {{ ref('raw_product') }} as p
 on o.productid = p.c2
+left join { ref('delivery_team') }} as d
+on o.shipmode = d.shipmode
 
 {{limit_data_in_dev('orderdate')}}
