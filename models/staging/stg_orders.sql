ca454d9956388cf9c8fdf2979f7975a65ea7865b
Tara Thankachan <taratraicy@gmail.com>
2024-12-15 14:10:13 +0000

    added macros

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -15,9 +15,12 @@ c.C4 as Country,
 p.C2 as productid,
 p.C1 as category,
 p.C3 as productname,
-p.C4 as subcategory
+p.C4 as subcategory,
+{{ markup('ordersellingprice','ordercostprice')}} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.C1
 left join {{ ref('raw_product') }} as p
 on o.productid = p.c2
+
+{{limit_data_in_dev('orderdate')}}
